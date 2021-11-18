9fe474b4c9636402e82455dff67be1a0fc2ab142
Victor T Weitz <tweitz20@gmail.com>
2021-11-17 16:48:54 -0800

    Added Javascript to my webpage!

---
+++ app1.js
@@ -0,0 +1,14 @@
+// console.log('Hello World this is the Hawks Nest!');
+
+// let userName = 'Victor';
+// console.log(userName);
+
+let userName = prompt('Enter you first name!');
+
+console.log(userName);
+//              'Welcome ' + 'Victor'
+if(userName == 'victor'){
+    document.write(`Welcome Your The Best! ${userName}`)
+} else {
+    document.write('Welcome To The Hawks Nest! ' + userName)
+}
---
+++ app3.js
@@ -0,0 +1,8 @@
+let anotherPicture = prompt('Would you like to see a picture of DK Metcalf?');
+let picURL = "https://s.yimg.com/ny/api/res/1.2/.y8rEKwbI_cO2sX7rFMsDA--/YXBwaWQ9aGlnaGxhbmRlcjt3PTk2MDtoPTYzOS45MTM1MzY4ODE5MjM4/https://s.yimg.com/os/creatr-uploaded-images/2020-12/1834b430-338b-11eb-9af3-93db761941b7"
+
+if(anotherPicture.toLowerCase() == "yes"){
+    // <img id="Metcalf" src="https://s.yimg.com/ny/api/res/1.2/.y8rEKwbI_cO2sX7rFMsDA--/YXBwaWQ9aGlnaGxhbmRlcjt3PTk2MDtoPTYzOS45MTM1MzY4ODE5MjM4/https://s.yimg.com/os/creatr-uploaded-images/2020-12/1834b430-338b-11eb-9af3-93db761941b7"
+
+    document.write('<img src="' + picURL + '">')
+}
