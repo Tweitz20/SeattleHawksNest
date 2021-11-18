af0e371cdbfe8f720fcf8ccdeb6ceb1d6f9dee1d
Victor T Weitz <tweitz20@gmail.com>
2021-11-17 17:00:18 -0800

    made another adjustment to app2.js

--- app2.js
+++ app2.js
@@ -1,6 +1,6 @@
 let userAnswer = prompt('Do you want to be a true fan? (yes or no)')
 // console.log(userAnswer);
 if(userAnswer.toLowerCase() == "yes"){
-    document.write('But now you are a fan of a losing season :)')
+    document.write('But now you are a fan of a losing season :(')
 }
 console.log(userAnswer);
