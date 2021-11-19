e4395e7d7207d1aff3dcc7e64331c1ee540d3ba3
Victor T Weitz <tweitz20@gmail.com>
2021-11-18 16:37:26 -0800

    changed my files to functions on app1.js

--- app1.js
+++ app1.js
@@ -1,14 +1,36 @@
-// console.log('Hello World this is the Hawks Nest!');
 
-// let userName = 'Victor';
-// console.log(userName);
+ function askUserQuestion(){
+    let userName = prompt('Enter you first name!');
 
-let userName = prompt('Enter you first name!');
-
-console.log(userName);
-//              'Welcome ' + 'Victor'
-if(userName == 'victor'){
-    document.write(`Welcome Your The Best! ${userName}`)
-} else {
-    document.write('Welcome To The Hawks Nest! ' + userName)
+    if(userName == 'victor'){
+        document.write(`Welcome Your The Best! ${userName}`)
+    } else {
+        document.write('Welcome To The Hawks Nest! ' + userName)
+    }
+    return userName;
+    // 'victor'
 }
+
+    let userNameOutside = askUserQuestion();
+
+function fanQuestion(){
+    let userAnswer = prompt('Do you want to be a true fan? (yes or no)')
+    // console.log(userAnswer);
+    if(userAnswer.toLowerCase() == "yes"){
+        document.write('But now you are a fan of a losing season :(')
+    }
+    console.log(userAnswer);
+}
+
+function anotherPicQuestion(){
+    let anotherPicture = prompt('Would you like to see a picture of DK Metcalf?');
+    let picURL = "https://s.yimg.com/ny/api/res/1.2/.y8rEKwbI_cO2sX7rFMsDA--/YXBwaWQ9aGlnaGxhbmRlcjt3PTk2MDtoPTYzOS45MTM1MzY4ODE5MjM4/https://s.yimg.com/os/creatr-uploaded-images/2020-12/1834b430-338b-11eb-9af3-93db761941b7"
+
+    if(anotherPicture.toLowerCase() == "yes"){
+        // <img id="Metcalf" src="https://s.yimg.com/ny/api/res/1.2/.y8rEKwbI_cO2sX7rFMsDA--/YXBwaWQ9aGlnaGxhbmRlcjt3PTk2MDtoPTYzOS45MTM1MzY4ODE5MjM4/https://s.yimg.com/os/creatr-uploaded-images/2020-12/1834b430-338b-11eb-9af3-93db761941b7"
+
+        document.write('<img src="' + picURL + '">')
+    }
+}
+
+anotherPictureQuestion();
---
+++ app4.js
@@ -0,0 +1,9 @@
+function addTwoNumbers(num1, num2){
+    let total = num1 + num2
+    console.log(total);
+    return total;
+}
+
+let number1 = addTwoNumbers(18, -15);
+console.log(number1);
+let number2 = addTwoNumbers(20, -10);
