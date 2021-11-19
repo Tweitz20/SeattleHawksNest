f447495c2dbb25b5f36dc7fed5de401efe041424
Victor T Weitz <tweitz20@gmail.com>
2021-11-19 09:25:55 -0800

    Added a for loop to my functiongit add .

--- app1.js
+++ app1.js
@@ -25,12 +25,9 @@ function fanQuestion(){
         ''
 
 function anotherPicQuestion(){
-    let anotherPicture = prompt('Would you like to see a picture of DK Metcalf?');
+    let anotherPicture = prompt('How many pictures of DK Metcalf would you like to see?');
     let picURL = "https://s.yimg.com/ny/api/res/1.2/.y8rEKwbI_cO2sX7rFMsDA--/YXBwaWQ9aGlnaGxhbmRlcjt3PTk2MDtoPTYzOS45MTM1MzY4ODE5MjM4/https://s.yimg.com/os/creatr-uploaded-images/2020-12/1834b430-338b-11eb-9af3-93db761941b7"
-
-    if(anotherPicture.toLowerCase() == "yes"){
-        // <img id="Metcalf" src="https://s.yimg.com/ny/api/res/1.2/.y8rEKwbI_cO2sX7rFMsDA--/YXBwaWQ9aGlnaGxhbmRlcjt3PTk2MDtoPTYzOS45MTM1MzY4ODE5MjM4/https://s.yimg.com/os/creatr-uploaded-images/2020-12/1834b430-338b-11eb-9af3-93db761941b7"
-
+    for(let i = 0; i < anotherPicture; i++){
         document.write('<img src="' + picURL + '">')
     }
 
